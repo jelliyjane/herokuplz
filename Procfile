48f176dfc330ccc94d5ebcbbb4a8cee806602ea4
jelliyjane <rean5123@naver.com>
2019-06-04 06:47:13 +0900

    second

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn mydreamproject.wsgi --log-file -
--- mydreamproject/settings.py
+++ mydreamproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +127,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
